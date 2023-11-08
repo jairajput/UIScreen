3d5c19ffba7d364d6d5859be8f12ca66dc25eedd
Jai <80166407+jairajput@users.noreply.github.com>
2023-11-08 23:22:23 +0530

    Initial Commit

---
+++ UIScreen/UIScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIScreenApp.swift
+//  UIScreen
+//
+//  Created by Jai  on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UIScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
